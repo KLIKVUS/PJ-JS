a754e44f7a2467c891946ba51802409cf653bb2a
admin <TimoninIV20@st.ithub.ru>
2021-02-15 22:42:40 +0300

    сделал LOGS

--- 090221/script.js
+++ 090221/script.js
@@ -1,28 +1,53 @@
+// Для активации
+let INF = false
+let getFORM = true
+
+
 // let form = document.forms[0]
 let form = document.forms.rating;
-console.log(form.elements);
+// console.log(form.elements);
+let table = document.body.firstElementChild.nextElementSibling
+let logText = document.createElement("p");
+table.appendChild(logText)
+logText.innerText = "LOGS:"
+let log1 = document.createElement("p");
+table.appendChild(log1)
+let tm = 0
+
 
 function getInfo() {
-    for (let i = 0; i < form.elements.length; i++) {
-        if (i !== form.elements.length - 1){
-            console.log(form.elements[i].value);
+    if (INF == true) {
+        for (let i = 0; i < form.elements.length; i++) {
+            if (i !== form.elements.length - 1){
+                console.log(form.elements[i].value);
+            }
+            // if (form.elements[i].type) {
+            //     console.log(form.elements[i].type);
+            // } else {
+            //     console.log(form.elements[i].value);
+            // }
         }
-        // if (form.elements[i].type) {
-        //     console.log(form.elements[i].type);
-        // } else {
-        //     console.log(form.elements[i].value);sssss
-        // }
     }
 }
 
 const getFormbody = function(el) {
-    console.log(el)
-    let formBody = {};
-    for (let i = 0; i < form.elements.length; i++) {
-        let el = form.elements[i];
-        if (el.name) {
-            formBody[el.name] = el.value;
+    if (getFORM == true) {
+        // console.log(el);
+        let formBody = [];
+        for (let i = 0; i < form.elements.length; i++) {
+            let el = form.elements[i];
+            if (el.name) {
+                tm++
+                if (tm == 3) {
+                    tm = 1
+                    log1.innerText = null;
+                }
+
+                formBody[el.name] = el.value;
+                log1.innerText += ` ${formBody[el.name]}`
+            }
+            console.log(formBody);
         }
+        // console.log(formBody);
     }
-    console.log(formBody);
 }
