87d42cf333edfc5579993cf4732ae46c0e8205b7
apa <timoniniv20@st.ithub.ru>
2021-02-16 17:19:26 +0300

    крако

--- 090221/script.js
+++ 090221/script.js
@@ -2,17 +2,17 @@
 let INF = false
 let getFORM = true
 
-
-// let form = document.forms[0]
+// Важные шуки
+// let formDiv = document.body
 let form = document.forms.rating;
-// console.log(form.elements);
-let table = document.body.firstElementChild.nextElementSibling
+// let form = document.forms[0];
+let table = document.body.firstElementChild.nextElementSibling;
 let logText = document.createElement("p");
-table.appendChild(logText)
-logText.innerText = "LOGS:"
+table.appendChild(logText);
+logText.innerText = "LOGS:";
 let log1 = document.createElement("p");
-table.appendChild(log1)
-let tm = 0
+table.appendChild(log1);
+let tm = 0;
 
 
 function getInfo() {
@@ -33,7 +33,7 @@ function getInfo() {
 const getFormbody = function(el) {
     if (getFORM == true) {
         // console.log(el);
-        let formBody = [];
+        let formBody = {};
         for (let i = 0; i < form.elements.length; i++) {
             let el = form.elements[i];
             if (el.name) {
@@ -46,8 +46,34 @@ const getFormbody = function(el) {
                 formBody[el.name] = el.value;
                 log1.innerText += ` ${formBody[el.name]}`
             }
-            console.log(formBody);
         }
         // console.log(formBody);
     }
+}
+
+
+let link = form.nextElementSibling;
+console.log(link);
+link.onclick = function(event) {
+    event.preventDefault();      // Отменить действие по умолчанию
+    link.style.color = getRGB();
+}
+
+form.onsubmit = function(e) {
+    e.preventDefault();
+    let formBody = {};
+    for (let i = 0; i < form.elements.length; i++) {
+        let el = form.elements[i];
+        if (el.name) {
+            formBody[el.name] = el.value  || "Пустота...";
+        }
+    }
+
+    console.log(formBody);
+
+    let info = document.createElement("div");
+    info.innerText = `${formBody.name} : ${formBody.shoto}`;
+    info.className = "line";
+    // document.body.insertBefore(info, form);
+    form.parentNode.insertBefore(info, form);   // Если есть папа
 }
