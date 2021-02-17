5e5416ce52cc8ee868321072216245dcc0b4baf8
admin <TimoninIV20@st.ithub.ru>
2021-02-17 21:16:14 +0300

    ну такое

--- 090221/script.js
+++ 090221/script.js
@@ -2,17 +2,20 @@
 let INF = false
 let getFORM = true
 
-
-// let form = document.forms[0]
+// Важные шуки
+// let formDiv = document.body
+// let form = document.forms[0];
 let form = document.forms.rating;
-// console.log(form.elements);
-let table = document.body.firstElementChild.nextElementSibling
+let table = document.body.firstElementChild.nextElementSibling;
+let list = document.getElementById("list");
+
 let logText = document.createElement("p");
-table.appendChild(logText)
-logText.innerText = "LOGS:"
+table.appendChild(logText);
+logText.innerText = "LOGS:";
 let log1 = document.createElement("p");
-table.appendChild(log1)
-let tm = 0
+table.appendChild(log1);
+
+let tm = 0;
 
 
 function getInfo() {
@@ -33,7 +36,7 @@ function getInfo() {
 const getFormbody = function(el) {
     if (getFORM == true) {
         // console.log(el);
-        let formBody = [];
+        let formBody = {};
         for (let i = 0; i < form.elements.length; i++) {
             let el = form.elements[i];
             if (el.name) {
@@ -46,8 +49,49 @@ const getFormbody = function(el) {
                 formBody[el.name] = el.value;
                 log1.innerText += ` ${formBody[el.name]}`
             }
-            console.log(formBody);
         }
         // console.log(formBody);
     }
 }
+
+
+let link = form.nextElementSibling;
+// console.log(link);
+link.onclick = function(event) {
+    event.preventDefault();      // Отменить действие по умолчанию
+    document.body.style.background = getRGB();
+}
+
+form.onsubmit = function(e) {
+    e.preventDefault();
+    let formBody = {};
+    for (let i = 0; i < form.elements.length; i++) {
+        let el = form.elements[i];
+        if (el.name) {
+            formBody[el.name] = el.value  || "Пустота...";
+        }
+    }
+
+    console.log(formBody);
+
+    list.innerText = `${formBody.name} : ${formBody.shoto}`;
+    list.className = "line";
+    
+    // document.body.insertBefore(info, form);
+    form.parentNode.insertBefore(info, form);   // Если есть папа
+}
+
+let selector = document.getElementById("shoto");
+let options = ["Шото", "шото", "ШоТо"]
+
+options.forEach(function(element, key) {
+    if (element == "Шото") {
+        selector[selector.options.length] = new Option(element, selector.length, false, false);
+    }
+    if (element == "шото") {
+        selector[selector.options.length] = new Option(element, selector.options.length, true, false);
+    }
+    if (element == "ШоТо") {
+        selector[selector.options.length] = new Option(element, selector.options.length, false, true);
+    }
+})
