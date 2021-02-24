3fa58eac1c765e4464819435c7dbddf97d29a1d6
admin <TimoninIV20@st.ithub.ru>
2021-02-24 23:49:13 +0300

    См

--- CM/180221/script.js
+++ CM/180221/script.js
@@ -1,34 +1,92 @@
-let head = document.getElementById("head");
+// Шапка и ее вложения
+let header = document.body.firstElementChild;
+let img = document.getElementById("img");
+// console.log(header);
+
+// Имечко
 let userName = document.createElement("h1");
 userName.innerText = "NAME";
-head.appendChild(userName);
-let form = document.getElementById("former");
-console.log(head);
+header.appendChild(userName);
 
+// Форма
+let form = document.getElementById("former");
+// console.log(former);
+
+// Картинки
 let logo = {
-    img: "svg\arrow-up.svg"
-    // img: ""
+    img1: "address-card.svg",
+    img2: "apple-alt.svg",
+    img3: "air-freshener.svg"
 }
 
 
+
+// Вписывалка имени
 form.onsubmit = function (es) {
     es.preventDefault();
     let formEl = [];
     for (let i = 0; i < form.elements.length; i++) {
         let ele = form.elements[i];
         if (ele.name) {
-            formEl[ele.name] = ele.value  || "ВАШЕ ИМЯ";
+            formEl[ele.name] = ele.value;
         }
     }
 
-    userName.innerText = formEl.name
-    console.log(formEl)
+    userName.innerText = formEl.name;
+    // console.log(formEl[name]);
 }
 
-options.forEach(function (element, key) {
-    if (element == "arrow-up") {
-        let imgUP = document.createElement("img")
-        head.appendChild(imgUP)
-        imgUP.src("svg\arrow-up.svg")
+// Выбор картинки
+seleCard.onclick = function() {
+    if (seleCard.value == "address-card") {
+        img.src = "svg/"+logo.img1;
+    } else if (seleCard.value == "apple-alt") {
+        img.src = "svg/"+logo.img2;
+    } else if (seleCard.value == "air-freshener") {
+        img.src = "svg/"+logo.img3;
     }
-})
+}
+
+// Смена цвета
+seleColor.onclick = function() {
+    if (seleColor.value == "Red") {
+        header.style.background = "#b10000d7";
+    } else if (seleColor.value == "Green") {
+        header.style.background = "#0d5f0d";
+    } else if (seleColor.value == "Blue") {
+        header.style.background = "#1616bd";
+    } else if (seleColor.value == "DEFAULT") {
+        header.style.background = "#02BA6A";
+    }
+}
+
+// Смена позиции
+let Left = 0;
+let Row = 0;
+checkLeft.onclick = function () {
+    if (checkLeft.checked && Row == 0) {
+        Left = 1;
+        header.style.alignItems = "flex-start";
+        userName.style.marginLeft = "5%";
+    } else if (checkLeft.checked && Row == 1) {
+        header.style.justifyContent = "left";
+    } else {
+        Left = 0;
+        header.style.alignItems = "center";
+        header.style.justifyContent = "center";
+        userName.style.marginLeft = "0";
+    }
+}
+checkRow.onclick = function () {
+    if (checkRow.checked && Left == 0) {
+        Row = 1;
+        header.style.flexDirection = "row";
+    } else if (checkRow.checked && Left == 1) {
+        header.style.flexDirection = "row";
+        // header.style.justifyContent = "left";
+    } else {
+        Row = 0;
+        header.style.flexDirection = "column";
+        header.style.justifyContent = "center";
+    }
+}
