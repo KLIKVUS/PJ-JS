53a5603e30a126ba93694c4a2f25e4b1730a5571
apa <timoniniv20@st.ithub.ru>
2021-03-30 17:13:57 +0300

    dada

--- 250321/script.js
+++ 250321/script.js
@@ -1,12 +1,11 @@
 const viewBox = document.getElementById("box"),
-items = document.getElementsByClassName("color-item");
+    bg = document.getElementById("bg"),
+    items = document.getElementsByClassName("color-item");
 /* 
     document.getElementsByClassName - получение HTML-коллекции из узлов элементов с заданным классом
     document.getElementsByName - получение HTML-коллекции из узлов элементов с атрибутом NAME
     document.getElementsByTagName - получение HTML-коллекции из узлов элементов с соответсвующим тегом
 */
-
-const bg = document.getElementsByClassName("bg");
 
 const getNumber = (max = 11, min = 0) => Math.floor(Math.random() * (max - min) + min);
 const getColor = () => `rgb(${getNumber(256)},${getNumber(256)},${getNumber(256)})`;
@@ -17,7 +16,7 @@ for(let it of items) {
     // .getAttribute("название атрибута") - получить значение атрибута
     let currentColor = it.getAttribute("data-color");
     it.setAttribute("style", `background: ${currentColor}`);
-    bg.setAttribute("style", `background: ${currentColor}`);
+    bg.setAttribute("style", `background: ${currentColor}`);    
     it.onclick = () => {
         viewBox.setAttribute("style", `background: ${currentColor}`);
         bg.setAttribute("style", `background: ${currentColor}`);
---
+++ 300321/script.js
@@ -0,0 +1,21 @@
+let items = document.getElementsByClassName("caption");
+items = Array.from(items)
+items.forEach(item => {
+    // classList - список всех классов элементов в виде массива
+    /*
+        Методы classList:
+        .add("class") - добавить элементу еще один класс
+        .remove("class") - удалить класс из элементов
+        .contains("class") - прповеряет наличие класса в теге
+        .toggle("class") - добавляет класс, елси его нет или удаляет класс, если он есть
+    */
+   item.onclick = (e) => {
+    e.target.classList.toggle("active");
+    let bro = e.target.nextElemetSibling;
+    if (e.target.classList.contains("active")) {
+        bro.style.height = bro.scrollHeight + "px";
+    } else {
+        bro.style.height = 0 + "px";
+    }
+   }
+});
