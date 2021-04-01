08be0ac2e3c670069a34de0f131089d1ced37a6a
apa <timoniniv20@st.ithub.ru>
2021-04-01 15:24:45 +0300

    da

---
+++ DATES/010421/script.js
@@ -0,0 +1,30 @@
+let ping = 0;
+// document.getElementsByTagName("button")[0];
+let btn = document.body.firstElementChild;
+
+// btn.onclick = function() {
+//     ping++;
+//     console.log("Click!", ping);
+// }
+
+// btn.onclick = function() {
+//     this.innerText = "Кнопка нажата!";
+// }
+
+const ticks = function() {
+    ping++;
+    console.log("Click!", ping);
+}
+
+btn.addEventListener("click", ticks);
+btn.addEventListener("click", function() {
+    this.innerText = "Кнопка нажата!";
+    setTimeout(function(){btn.innerText="Жмякус"}, 500);
+    if (ping >= 10) {
+        alert("А уже усе");
+        btn.removeEventListener("click", ticks);
+    }
+});
+btn.addEventListener("click", e => {
+    document.body.style.opacity = ((100 - ping * 10) / 100);
+});
