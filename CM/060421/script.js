3ae51acd7edd38f809eaec8a9cfc642d9cc13ea0
admin <TimoninIV20@st.ithub.ru>
2021-04-07 19:59:36 +0300

    da

--- CM/060421/script.js
+++ CM/060421/script.js
@@ -1,5 +1,32 @@
-const op = document.getElementById("op");
+let op = document.getElementsByClassName("op");
+op = Array.from(op);
 
-op.addEventListener("click", function(e) {
-    // e.target.style.height = "auto";
+op.forEach(item => {
+    let bro = item.parentNode;
+    let h = item.firstElementChild;
+    bro.style.height = h.scrollHeight + "px"
+    item.onclick = e => {
+        if (bro.style.height != "auto") {
+            bro.style.height = "auto";
+            bro.style.overflow = "visible";
+        } else {
+            bro.style.height = e.target.scrollHeight + "px";
+            bro.style.overflow = "hidden";
+        }
+    }
+
+
+//          Бяка кривая
+    // item.onclick = e => {
+    //     let bro = e.target.parentNode.parentNode;
+    //     bro.classList.toggle("active");
+    //     if (bro.classList.contains("active")) {
+    //         bro.style.height = bro.scrollHeight + "px";
+    //         bro.style.overflow = "visible";
+    //     } else {
+    //         bro.style.height = e.target.scrollHeight + "px";
+    //         bro.style.overflow = "hidden";
+    //     }
+    // }
 })
+
--- DATES/300321/script.js
+++ DATES/300321/script.js
@@ -1,7 +1,18 @@
 let items = document.getElementsByClassName("caption");
 items = Array.from(items)
 items.forEach(item => {
-    // classList - список всех классов элементов в виде массива
+    item.onclick = e => {
+        e.target.classList.toggle("active");
+        let bro = e.target.nextElementSibling;
+        if (e.target.classList.contains("active")) {
+            bro.style.height = bro.scrollHeight + "px";
+        } else {
+            bro.style.height = 0 + "px";
+        }
+    }
+});
+
+// classList - список всех классов элементов в виде массива
     /*
         Методы classList:
         .add("class") - добавить элементу еще один класс
@@ -9,13 +20,3 @@ items.forEach(item => {
         .contains("class") - прповеряет наличие класса в теге
         .toggle("class") - добавляет класс, елси его нет или удаляет класс, если он есть
     */
-   item.onclick = (e) => {
-    e.target.classList.toggle("active");
-    let bro = e.target.nextElemetSibling;
-    if (e.target.classList.contains("active")) {
-        bro.style.height = bro.scrollHeight + "px";
-    } else {
-        bro.style.height = 0 + "px";
-    }
-   }
-});
