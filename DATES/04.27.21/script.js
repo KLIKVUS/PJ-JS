933be950823318752a8947e05a798074da3bf144
Your Name <you@example.com>
2021-05-04 16:55:09 +0300

    s

--- DATES/04.27.21/script.js
+++ DATES/04.27.21/script.js
@@ -10,4 +10,35 @@ window.addEventListener("load", e => {
         el.style.backgroundImage = `url(${img})`;
         container.appendChild(el);
     });
+
+
+    const lbx = document.querySelector(".lightbox");
+    const cross = lbx.querySelector(".cross");
+    const next = lbx.querySelector("arrow.right");
+    const prev = lbx.querySelector("arrow.left");
+    const card = document.querySelectorAll(".card");
+
+    const showLightbox = (e) => {
+        document.body.classList.add("light")
+        lbx.firstElementChild.style.backgroundImage = `url(${e.target.getAttribute("data-src")})`
+    }
+    const hideLightbox = () => {
+        document.body.classList.remove("light")
+    }
+    const nextI = () => {
+        lbx.firstElementChild.style.backgroundImage = `url(${e.target.getAttribute("data-src")})`
+    }
+
+    card.forEach(card => {
+        card.addEventListener("click", showLightbox);
+    });
+    cross.addEventListener("click", hideLightbox);
+    document.body.addEventListener("keydown", (e) => {
+        // console.log(e.key);
+        if (e.key === "Escape") {
+            hideLightbox();
+        }
+    })
+
+    next.addEventListener("click", )
 });
